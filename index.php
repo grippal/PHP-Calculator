2041cdac57c6efe4ca01e0aadbaae742772b0531
grippal <lucagrip@yahoo.com>
2020-06-12 13:34:55 -0400

    add, subtract, multiply, divide functionality added

--- index.php
+++ index.php
@@ -18,23 +18,53 @@
 	<meta name="viewport"
 	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Unit Conversion Tool</title>
+	<title>Calculator</title>
 </head>
 <body>
 <div>
-	<h1>Unit Conversion Tool</h1>
+	<h1>Calculator</h1>
 </div>
 <main>
-	<p>Convert units of your choice!</p>
-	<form class="uc-form" action="index.php" method="post">
-		<input type="number" name="input1" placeholder="0.00">
-		<input type="number" name="input2" placeholder="0.00">
-		<button type="submit" name="convert">Convert</button>
+	<form class="calc-form" action="index.php" method="post">
+		<input type="text" name="num1" placeholder="Number 1">
+        <input type="text" name="num2" placeholder="Number 2">
+        <select name="operator" id="">
+            <option value="">None</option>
+            <option value="">Add</option>
+            <option value="">Subtract</option>
+            <option value="">Multiply</option>
+            <option value="">Divide</option>
+        </select>
+        <br>
+		<button type="submit" name="submit" value="submit">Calculate</button>
 	</form>
 
+    <p>The answer is:</p>
+
     <?php
-	    echo $_POST['input1'];
-	    echo $_POST['input2'];
+        if (isset($_POST['submit'])) {
+            $results1 = $_POST['num1'];
+	        $results2 = $_POST['num2'];
+	        $operator = $_POST['operator'];
+
+	        switch ($operator) {
+                case "None":
+                    echo "You need to select an operator!";
+                break;
+		        case "Add":
+			        echo $results1 + $results2;
+			    break;
+		        case "Subtract":
+			        echo $results1 - $results2;
+			    break;
+		        case "Multiply":
+			        echo $results1 * $results2;
+			    break;
+		        case "Divide":
+			        echo $results1 / $results2;
+			    break;
+	        }
+        }
     ?>
 </main>
 
